cb888c28277f3403a0f1c093fdedc30c70a328a3
Dimitar-Totov <dimitar.totov@abv.bg>
2025-02-21 20:45:03 +0200

    Add homepage template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import HomeSection from "./components/home/HomeSection";
+import TopAreaSection from "./components/home/TopArea";
 export default function App() {
 
   return (
     <>
-      
+      <TopAreaSection />
+      <HomeSection />
     </>
   )
 }
---
+++ client/src/components/home/HomeSection.jsx
@@ -0,0 +1,31 @@
+export default function HomeSection() {
+    return (
+        <section id="home" className="welcome-hero">
+            <div className="welcome-hero-serch-box">
+                <div className="welcome-hero-form">
+                    <div className="single-welcome-hero-form">
+                        <form>
+                            <input type="text" placeholder="Location" />
+                        </form>
+                    </div>
+                    <div className="welcome-hero-serch">
+                        <button className="welcome-hero-btn" onclick="window.location.href='#'">
+                            search  <i data-feather="search"></i>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="container">
+                <div className="welcome-hero-txt">
+                    <h2>best place to find and explore that all you need </h2>
+                    <p>
+                        Find the best places you can visit
+                    </p>
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
